6018bf2a611d3889884297aee2e705f45e84893c
Jyoti Singh <singhjyoti010@outlook.com>
2024-09-13 11:57:53 +0530

    updatedlogin method

--- features/pageobjects/login.page.ts
+++ features/pageobjects/login.page.ts
@@ -16,7 +16,15 @@ export class LoginPage {
         await loginControls.emailInputField.setValue(username);
         await loginControls.passwordInputField.setValue(password);
         await loginControls.btnSubmit.click();
-        await homeControls.userHeader.waitForDisplayed({timeout: 20000});
-        // await expect.(homeControls.userHeader).toBeDisplayed();
+        if(await $('.zm-message__content').isDisplayed()){
+            await browser.pause(5000);
+            await loginControls.btnSubmit.click();
+        }
+        await browser.saveScreenshot('./some/loginscreenshot.png');
+        // await browser.pause(50000);
+        console.log('hi all' + await homeControls.userHeader.isExisting());
+        await browser.saveScreenshot('./some/screenshot.png');
+        await homeControls.userHeader.waitForDisplayed({timeout: 30000});
+        console.log(await homeControls.userHeader.isExisting());
     }
 }
